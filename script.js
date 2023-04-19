4438007d896edc099831efb7340c3b829e0161e4
rau1avi1a <avila0118@icloud.com>
2023-04-18 22:12:54 -0700

    added functionality to buttons and established local storage

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-var eventsData
+var eventsData = {};
 
+var savedData = {};
+
+
+var today = moment();
+
+
+//This function sets the color block based on the time
 function setHourColors() {
-    var now = dayjs();
+    var now = moment();
 
     for (var i = 9; i< 18; i++) {
         if (i < now.hour()) {
-            $("#hour" + i + " textarea").addClass("past");
+            $("#hour-" + i).addClass("past");
         }
         else if (i == now.hour()) {
-            $("#hour" + i + " textarea").addClass("present");
+            $("#hour-" + i).addClass("present");
         }
         else if (i > now.hour()) {
-            $("#hour" + i + " textarea").addClass("future");
+            $("#hour-" + i).addClass("future");
         }
     }
 }
 
 function loadStoredData() {
     eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
-    if (!eventsData) {
+    if (eventsData !== null) {
+        savedData = eventsData;
+    }
+    else {
         eventsData = {
             hour9: "",
-            hour10: ""
+            hour10: "",
+            hour11: "",
+            hour12: "",
+            hour13: "",
+            hour14: "",
+            hour15: "",
+            hour16: "",
+            hour17: "",
         }
     }
+    generateTable();
+}
 
+function generateTable() {
+    for (var i = 9; i < 18; i++) {
+        var textBlock = $('#hour' + i);
+        textBlock.text(savedData["hour" + i]);
+    }
 }
 
 function handleSaveClick(event) {
@@ -41,9 +65,12 @@ function handleSaveClick(event) {
 }
 
 
-$('.saveBtn').on('click', )
+$('.saveBtn').on('click', handleSaveClick);
 
+//Set current date on page
+$('#currentDay').text(today.format('MMMM Do YYYY'));
 
+//initial functions
 $(function() {
     loadStoredData();
     setHourColors();
